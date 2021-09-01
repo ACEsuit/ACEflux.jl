2390590644e0f2f517a04cd127280e223d51830d
Andres Ross <andresrossb@gmail.com>
2021-09-01 15:25:47 -0700

    some small changes

--- test/preconLinACE.jl
+++ test/preconLinACE.jl
@@ -5,7 +5,7 @@ using JuLIP
 #to change the size of Psi
 #   size of data
 # a.- change the number of atoms in the structure 
-# b.- change the size of each atom
+# b.- change the size of each atom, also affects size of structure
 #   size of parameters
 # c.- change maxdeg 
 
@@ -13,8 +13,9 @@ using JuLIP
 #create several atoms
 structure = []
 for _ in 1:100 # a.-
-   at = bulk(:Si, cubic=true) * 2 # b.-
-   rattle!(at,0.6)
+   at = bulk(:Si, cubic=true) * 3 # b.-
+   r1 = rand() * 0.6
+   rattle!(at, r1)
    set_calculator!(at, StillingerWeber())
    push!(structure, at)
 end
@@ -22,7 +23,7 @@ end
 #make our basis
 maxdeg = 12 # c.-
 ord = 3
-B1p = ACE.Utils.RnYlm_1pbasis(; maxdeg=maxdeg, D = NaiveTotalDegree())
+B1p = ACE.Utils.RnYlm_1pbasis(; maxdeg=maxdeg, D = NaiveTotalDegree(),  rcut = cutoff(StillingerWeber()))
 pibasis = PIBasis(B1p, ord, maxdeg; property = ACE.Invariant())
 basis = SymmetricBasis(pibasis, ACE.Invariant());   
 
@@ -32,8 +33,7 @@ y = zeros(length(structure)*length(structure[1]))
 #loop over the atoms
 idx = 0
 for at in structure
-   #0.9 so that the structures are slightly larger than the radius used
-   nlist = neighbourlist(at, 0.9 * cutoff(StillingerWeber()))
+   nlist = neighbourlist(at, cutoff(StillingerWeber()))
    for i = 1:length(at)
       Js, tmpRs, Zs = JuLIP.Potentials.neigsz(nlist, at, i); z0 = at.Z[i]
       Rs = ACEConfig([State(rr = tmpRs[j]) for j in 1:length(tmpRs)])
