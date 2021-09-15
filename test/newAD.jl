be0dc6af52cb1428b2ff8609e5ecf193cc07f78b
Andres Ross <andresrossb@gmail.com>
2021-09-15 10:07:21 -0700

    fixed grad_params_config
    It now runs and optimizes. It still need to be tested and the grad_params_config derivative needs to be taken through adjoints. For the latter will need to coordinate with ACE.jl

--- test/newAD.jl
+++ test/newAD.jl
@@ -44,18 +44,19 @@ function Energy_ACE(maxdeg, ord, Nprop)
    basis = SymmetricBasis(pibasis, ACE.Invariant());   
 
    #create a multiple property model
-   W = rand(Nprop, length(basis))  #matrix for Flux
+   W = rand(Nprop, length(basis))  #matrix for Flux 
    Wsv =  [SVector{size(W)[1]}(W[:,i]) for i in 1:size(W)[2]]  #SVector for ACE
    LM = LinearACEModel(basis, Wsv, evaluator = :standard) 
    return Energy_ACE(W,LM)
 end
 
-@functor Energy_ACE
-
+@functor Energy_ACE #so that flux can find the parameters
+ 
+#forward pass
 (y::Energy_ACE)(at) = _eval_energy_ACE(y.weight, y.m, at)
 
-function _eval_energy_ACE(Wt, M, at)
-   W = [SVector{size(Wt)[1]}(Wt[:,i]) for i in 1:size(Wt)[2]] #SVector conversion
+#neighboor list finder, It's here so we can indicate that it should not be differentiated
+function neighbourfinder(at)
    Rs = []
    nlist = neighbourlist(at, cutoff(EMT()))
    for i = 1:length(at)
@@ -63,64 +64,82 @@ function _eval_energy_ACE(Wt, M, at)
       tmpRs=ACEConfig([State(rr = tmpRs[j]) for j in 1:length(tmpRs)])
       append!(Rs,[tmpRs])
    end 
+   return Rs
+end
+
+#a hack to avoid derviating neighbourfinder, this will be done differently 
+function ChainRules.rrule(::typeof(neighbourfinder), at)
+   return neighbourfinder(at), dn -> dn
+end
+
+#energy evaluation
+function _eval_energy_ACE(Wt, M, at)
+   W = [SVector{size(Wt)[1]}(Wt[:,i]) for i in 1:size(Wt)[2]] #SVector conversion
    set_params!(M, W)
+   Rs = neighbourfinder(at)
    E = sum([getproperty.(evaluate(M ,r), :val) for r in Rs])
    return E
 end
 
 #adj is outside chainrule so we can define a chainrule for it
-function adj(dp, W, M, Rs)
-   set_params!(M, W) #might not be necesary
+function adj(dp, Wt, M, Rs)
 
-   #It is this complicated because of SVector conversions
-   gparams = sum([grad_params(M ,r) for r in Rs])
+   W = [SVector{size(Wt)[1]}(Wt[:,i]) for i in 1:size(Wt)[2]]  #SVector conversion
+   set_params!(M, W) 
+
+   #Params derivative
+   gparams = sum([grad_params(M ,r) for r in Rs]) #gradient is returned as SVector for now
+   #make them numbers rather than invariants, this should be different
    grad = zeros(SVector{length(gparams[1])},length(gparams))
    for i = 1:length(gparams)
       grad[i] = getproperty.(gparams[i], :val)
    end
+   #multiply by dp
    temp_grad = [dp .* grad[i] for i in 1:length(grad)]
+   #we convert our SVector into a matrix
    gradMatrix = zeros(length(temp_grad[1]), length(temp_grad))
    for i in 1:length(temp_grad)
       gradMatrix[:,i] = temp_grad[i]
    end
 
+   #Forces
+   #TODO do we want to multiply dp?
    gconfig = [sum(grad_config(M,r)) for r in Rs]
-   @show length(gradMatrix)
-   @show size(gradMatrix)
+  
    return (NoTangent(), gradMatrix, NoTangent(), gconfig)
 end
 
 function ChainRules.rrule(::typeof(_eval_energy_ACE), Wt, M, at)
-   W = [SVector{size(Wt)[1]}(Wt[:,i]) for i in 1:size(Wt)[2]]  #SVector conversion
-   Rs = []
-   nlist = neighbourlist(at, cutoff(EMT()))
-   for i = 1:length(at)
-      Js, tmpRs, Zs = JuLIP.Potentials.neigsz(nlist, at, i); z0 = at.Z[i]
-      tmpRs=ACEConfig([State(rr = tmpRs[j]) for j in 1:length(tmpRs)])
-      append!(Rs,[tmpRs])
-   end 
-   set_params!(M, W)
-   E = sum([getproperty.(evaluate(M ,r), :val) for r in Rs])
-   
-   return E, dp -> adj(dp, W, M, Rs)
+   Rs = neighbourfinder(at)
+   E = _eval_energy_ACE(Wt, M, at)
+   return E, dp -> adj(dp, Wt, M, Rs)
 end
 
 #it actually returns all the gradient combinations. So der of parameters twice, of model
 #twice of model and parameters, etc. However, we can just pass NoTangent() when we don't need them
-# function ChainRules.rrule(::typeof(adj), dp, W, M, Rs)
+function ChainRules.rrule(::typeof(adj), dp, W, M, Rs)
 
-#    function secondAdj(dq)
+   #this is not fast, but we can change this function to return or calculate
+   #exactly what we want, so theoretically could be optimized.
+   #all the sum() work because the loss only sums, need to think
+   #exactly about how to pull the matrix together, but ideally we wouldn't even compute the matrix.
+   function secondAdj(dq)
+      #sum over all r in Rs
+      cumsum = zeros(length(M.c[1]), length(M.c))
+      for (ri,r) in enumerate(Rs)
+         Temp = grad_params_config(M, r)
+         #sum over config length
+         Temp = [sum([Temp[prop][:,i] for i in 1:length(Temp[prop][1,:])]) for prop in 1:length(M.c[1])]
+         for prop in 1:length(M.c[1])
+            #multiply by dq and sum over x,y,z
+            cumsum[prop, :] += [sum(dq[4][ri].rr .* Temp[prop][i].rr) for i in 1:length(Temp[prop])]
+         end
+      end
 
-#       @show length(dq[4]) #108 svectors of 3
-#       gpc = [sum(grad_params_config(M, r)) for r in Rs]
-#       @show length(gpc)
-#       @show length(gpc[1])
-#       @show dq[4][1]
-
-#       return(NoTangent(), NoTangent(), "gradConfigParams", NoTangent(), NoTangent())
-#    end
-#    return(adj(dp, W, M, Rs), secondAdj)
-# end
+      return(NoTangent(), NoTangent(), cumsum, NoTangent(), NoTangent())
+   end
+   return(adj(dp, W, M, Rs), secondAdj)
+end
 # # ------------------------------------------------------------------------
 # #    create a flux model with a few layers
 # # ------------------------------------------------------------------------
@@ -132,18 +151,18 @@ GRAD(model, x) = Zygote.gradient( x -> EVAL(model, x), x )[1]
 
 ## define the loss
 #done this way to substract SVector and state objects
-
 function loss(x,y)
    Ftemp = GRAD(model, x)
    return(abs2(EVAL(model, x) - y.E) + sum([sum(Ftemp[i].rr - y.F[i]) for i in 1:length(y.F)]))
 end
+
 ## optimize
 opt = Descent()
 data = zip(Xtrain, Ytrain)
 total_loss = () -> sum(loss(x, y) for (x, y) in data)
 @show total_loss()
-# p = params(model)
+p = params(model)
 
-# Flux.train!(loss, p, data, opt)
-# @show total_loss()
+Flux.train!(loss, p, data, opt)
+@show total_loss()
 
