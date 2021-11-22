f8fa42e9c94ef825ba161cce62e147d766519b48
andresrossb <andresrossb@gmail.com>
2021-11-22 12:19:46 -0800

    logging bson for opt_flux

--- src/training.jl
+++ src/training.jl
@@ -2,6 +2,7 @@ using Zygote
 using StatsBase
 using ThreadTools
 using Folds
+using BSON: @save
 
 function gsum(grads)
    gr = first(grads)
@@ -17,7 +18,7 @@ end
 #loss function, parameters in the form of params(model), X input samples, Y targets
 #the optimizer (flux object), the size of the batch, number of epochs,
 #multi threading or single thread (boolean)
-function opt_Flux(loss, θ, X, Y, opt, epochs; b=length(X))
+function opt_Flux(loss, θ, X, Y, opt, epochs, nthrds, fname; b=length(X), lograte = 50)
    trn_loss = Float64[]
    gradN = Float64[]
    append!(trn_loss, mean(loss.(X,Y)))
@@ -25,35 +26,40 @@ function opt_Flux(loss, θ, X, Y, opt, epochs; b=length(X))
 
    #our batch size, saved as a mutable array of length b
    indx = zeros(Int64,b)
-
+   tic = time()
    for e in 1:epochs
+
       #we sample our training data and get the gradient
       StatsBase.sample!(1:length(X), indx; replace=false)
 
-      if(e%2==0)
-         #tic = time()
-         g = gsum(tmap((x,y) -> Zygote.gradient(()->loss(x,y), θ), X[indx], Y[indx]))
+      if(e%lograte==0)
+
+         g = gsum(tmap((x,y) -> Zygote.gradient(()->loss(x,y), θ), nthrds, X[indx], Y[indx]))
          
          Flux.Optimise.update!(opt, θ, g)
       
-         append!(trn_loss, sum(tmap((x,y) -> loss(x,y), X,Y)) / length(X))
+         append!(trn_loss, sum(tmap((x,y) -> loss(x,y), nthrds, X,Y)) / length(X))
          for gp in g #L1 norm, (g is a Flux gradient object)
             n += sum(abs.(gp))
          end
          append!(gradN,n)
-         #toc = time()
+
          @show "#############################"
-         @show e
-         #@show (epochs - e)*(toc - tic) / 3600
+         @show (epochs - e)*(time() - tic) / (lograte*3600)
          @show last(trn_loss)
+
+         @save string(fname, ".bson") θ
+
+         tic = time()
+
       else
          #both have similar performance
-         g = gsum(tmap((x,y) -> Zygote.gradient(()->loss(x,y), θ), X[indx], Y[indx]))
+         g = gsum(tmap((x,y) -> Zygote.gradient(()->loss(x,y), θ), nthrds, X[indx], Y[indx]))
          #g = Folds.mapreduce((x,y) -> Zygote.gradient(()->loss(x,y), θ), +, X[indx], Y[indx])
 
          Flux.Optimise.update!(opt, θ, g)
       
-         append!(trn_loss, sum(tmap((x,y) -> loss(x,y), X,Y)) / length(X))
+         append!(trn_loss, sum(tmap((x,y) -> loss(x,y), nthrds, X,Y)) / length(X))
          for gp in g #L1 norm, (g is a Flux gradient object)
             n += sum(abs.(gp))
          end
